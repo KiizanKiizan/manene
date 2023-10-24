c6914859e6a9889a0d0b459b995ca100d747265e
Haruka <hfujii1013@gmail.com>
2023-10-23 19:23:54 -0700

    feat: SizeMeasurementsPageの作成

---
+++ app/size_measurements/page.tsx
@@ -0,0 +1,5 @@
+import { SizeMeasurementsContainer } from '../_components/size_measurements/size-measurements-container';
+
+export default function SizeMeasurementsPage() {
+  return <SizeMeasurementsContainer />;
+}
