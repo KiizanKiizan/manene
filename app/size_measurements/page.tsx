3c1a7974238719ab06cfc67349e0d79985279e91
Haruka <hfujii1013@gmail.com>
2023-10-24 19:45:09 -0700

    fix: <></>削除

--- app/size_measurements/page.tsx
+++ app/size_measurements/page.tsx
@@ -1,9 +1,5 @@
 import { SizeMeasurementsContainer } from '../_components/size_measurements/size-measurements-container';
 
 export default function SizeMeasurementsPage() {
-  return (
-    <>
-      <SizeMeasurementsContainer />
-    </>
-  );
+  return <SizeMeasurementsContainer />;
 }
