c2431dc0b32e5073a8d6b176182084b9ac346669
Haruka <hfujii1013@gmail.com>
2023-10-24 17:57:54 -0700

    feat: SizeMeasurementsListItemの作成。

--- app/_components/size_measurements/[id]/size-measurements-list-item.tsx
+++ app/_components/size_measurements/[id]/size-measurements-list-item.tsx
@@ -1,7 +1,7 @@
 // ListItem
 'use client';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import { ListItem, Typography } from '@mui/material';
+import { Box, ListItem, Typography, useTheme } from '@mui/material';
 import { LiaHandPointRight } from 'react-icons/lia';
 
 export type TOptionDetail = {
@@ -23,16 +23,48 @@ export default function SizeMeasurementsListItem({
   isSelected,
   onClickSelect,
 }: TProps) {
+  const theme = useTheme();
   return (
-    <ListItem sx={{ display: 'flex' }}>
+    <ListItem
+      divider
+      sx={{
+        display: 'flex',
+        width: '100%',
+        height: '6rem',
+        justifyContent: 'space-around',
+      }}>
       <LiaHandPointRight
-        color={isSelected ? 'warning.main' : undefined}
+        size={27}
+        color={isSelected ? 'red' : undefined}
         onClick={() => onClickSelect(optionDetail.id)}
       />
-      <Typography>{optionDetail.partName}</Typography>
-      <Typography>{optionDetail.preMeasurement}</Typography>
-      <ArrowRightIcon />
-      <Typography>{optionDetail.newMeasurement ?? ''}</Typography>
+      <Typography
+        style={{
+          color: theme.palette.primary.main,
+          fontSize: '1.2rem',
+          fontWeight: '500',
+        }}>
+        {optionDetail.partName}
+      </Typography>
+      <Typography
+        style={{ fontSize: '1.2rem', color: theme.palette.secondary.main }}>
+        {optionDetail.preMeasurement}cm
+      </Typography>
+      <ArrowRightIcon style={{ color: theme.palette.secondary.main }} />
+      <Box>
+        <Typography
+          style={{
+            color: theme.palette.primary.main,
+            fontSize: '0.8rem',
+            fontWeight: '300',
+          }}>
+          {optionDetail.actionMessage}
+        </Typography>
+        <Typography
+          style={{ fontSize: '1.2rem', color: theme.palette.secondary.main }}>
+          {optionDetail.newMeasurement ?? ''}cm
+        </Typography>
+      </Box>
     </ListItem>
   );
 }
--- app/size_measurements/page.tsx
+++ app/size_measurements/page.tsx
@@ -1,5 +1,24 @@
+'use client';
 import { SizeMeasurementsContainer } from '../_components/size_measurements/size-measurements-container';
+import SizeMeasurementsListItem from '../_components/size_measurements/[id]/size-measurements-list-item';
 
 export default function SizeMeasurementsPage() {
-  return <SizeMeasurementsContainer />;
+  return (
+    <>
+      <SizeMeasurementsContainer />
+      <SizeMeasurementsListItem
+        optionDetail={{
+          id: 0,
+          partName: 'ヒップ',
+          preMeasurement: 100,
+          newMeasurement: 150,
+          actionMessage: 'スキップ',
+        }}
+        isSelected={true}
+        onClickSelect={function (id: number): void {
+          throw new Error('Function not implemented.');
+        }}
+      />
+    </>
+  );
 }
