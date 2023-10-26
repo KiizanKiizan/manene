144b24e5d483235ea09d0c8e925ec033c724fc6e
Haruka <hfujii1013@gmail.com>
2023-10-26 15:52:04 -0700

    feat: add buttons

--- app/_components/size_measurements/[id]/Size-measurements-edit-form.tsx
+++ app/_components/size_measurements/[id]/Size-measurements-edit-form.tsx
@@ -2,6 +2,7 @@
 
 import {
   Box,
+  Button,
   MenuItem,
   Select,
   SelectChangeEvent,
@@ -18,6 +19,7 @@ type TProps = {
   optionDetails: TOptionDetail[];
   needPartsForSizeCalc: number[];
   selectedPartdId: number;
+  isLoading: boolean;
   onChangeMeasurement: (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => void;
@@ -25,6 +27,8 @@ type TProps = {
   onChangeRank: (e: SelectChangeEvent<string>) => void;
   onClickEnter: (e: KeyboardEvent) => void;
   onClickSelect: (id: number) => void;
+  onClickUpdate: () => void;
+  onClickSkip: () => void;
 };
 
 export default function SizeMeasurementsEditForm({
@@ -32,50 +36,83 @@ export default function SizeMeasurementsEditForm({
   rank,
   optionDetails,
   selectedPartdId,
+  isLoading,
   onChangeMeasurement,
   onChangeSize,
   onChangeRank,
   onClickEnter,
   onClickSelect,
+  onClickUpdate,
+  onClickSkip,
 }: TProps) {
   return (
     <>
-      <Box>
-        <Typography style={{ fontSize: '1.2rem' }}>ウエストの測定</Typography>
+      <Box sx={{ p: '1.2rem' }}>
+        <Box
+          color="secondary.main"
+          sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography
+            variant="h6"
+            style={{ fontWeight: '300' }}>
+            <span style={{ fontWeight: '500' }}>ウエスト</span>
+            の測定
+          </Typography>
 
-        <TextField
-          id="standard-basic"
-          variant="standard"
-          onChange={onChangeMeasurement}
-          onKeyDown={(e) => onClickEnter}
+          <TextField
+            id="standard-basic"
+            variant="standard"
+            onChange={onChangeMeasurement}
+            onKeyDown={(e) => onClickEnter}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'flex-start',
+            borderBottom: 1,
+          }}>
+          <Typography
+            variant="h6"
+            color="secondary.main"
+            sx={{ mr: '1.5rem' }}>
+            サイズ
+          </Typography>
+          <Select
+            value={size}
+            onChange={onChangeSize}>
+            <MenuItem value="S">S</MenuItem>
+            <MenuItem value="M">M</MenuItem>
+            <MenuItem value="L">L</MenuItem>
+            <MenuItem value="XL">XL</MenuItem>
+          </Select>
+        </Box>
+        <SizeMeasurementsList
+          optionDetails={optionDetails}
+          selectedPartId={selectedPartdId}
+          onClickSelect={onClickSelect}
         />
-      </Box>
-      <Box>
-        <Typography>サイズ</Typography>
-        <Select
-          value={size}
-          onChange={onChangeSize}>
-          <MenuItem value="S">S</MenuItem>
-          <MenuItem value="M">M</MenuItem>
-          <MenuItem value="L">L</MenuItem>
-          <MenuItem value="XL">XL</MenuItem>
-        </Select>
-      </Box>
-      <SizeMeasurementsList
-        optionDetails={optionDetails}
-        selectedPartId={selectedPartdId}
-        onClickSelect={onClickSelect}
-      />
-      <Box>
-        <Typography>ランク</Typography>
-        <Select
-          value={rank}
-          onChange={onChangeRank}>
-          <MenuItem value="S">S</MenuItem>
-          <MenuItem value="A">A</MenuItem>
-          <MenuItem value="B">B</MenuItem>
-        </Select>
+        <Box>
+          <Typography>ランク</Typography>
+          <Select
+            value={rank}
+            onChange={onChangeRank}>
+            <MenuItem value="S">S</MenuItem>
+            <MenuItem value="A">A</MenuItem>
+            <MenuItem value="B">B</MenuItem>
+          </Select>
+        </Box>
+        <Box>
+          <Button onClick={onClickSkip}>スッキプ</Button>
+          <Button
+            onClick={onClickUpdate}
+            disabled={isLoading}>
+            確定
+          </Button>
+        </Box>
       </Box>
     </>
   );
 }
+
+//全てが入力されていないと確定ボタンを押せないようにする。
+//ウエストの箇所を動的にする
