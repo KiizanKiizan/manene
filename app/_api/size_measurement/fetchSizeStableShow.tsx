ffca00f94253a50a5d188270a37223f97b9e550d
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-11-18 10:22:47 +0900

    fix: 型名を修正

--- app/_api/size_measurement/fetchSizeStableShow.tsx
+++ app/_api/size_measurement/fetchSizeStableShow.tsx
@@ -1,12 +1,13 @@
-import fetchData from '../fetchData';
+import fetchData from "../fetchData";
 
 type TParams = { itemId: number };
-export type TGetSizeMeasurementIndexResponse = {
+
+export type TSizeStableShowResponse = {
   isSizeStableAfterMeasurement: boolean;
 };
 
 export default async function fetchSizeStableShow(params: TParams) {
-  return fetchData<TGetSizeMeasurementIndexResponse, TParams>({
+  return fetchData<TSizeStableShowResponse, TParams>({
     path: `items/${params.itemId}/size_stable_count`,
     params: params,
   });
