a85ba2630086e2232a841c34e2b5ce560c5e493e
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-11-25 08:56:27 +0900

    fix: 型の修正

--- app/_api/item_register/form/useSizeMeasurementInputsIndex.ts
+++ app/_api/item_register/form/useSizeMeasurementInputsIndex.ts
@@ -1,8 +1,25 @@
-import { TMeasurementInput as TSizeMeasurementInputsIndexResponse } from "../../size_measurement/getSizeMeasurementIndex";
 import { useGetRequest } from "../../useGetRequest";
 
 type TParams = {
   stockingOrderId: number;
+};
+
+export type TMeasurement = {
+  part: number;
+  value: number | null;
+};
+
+export type TMeasurementInput = {
+  mCateSmallId: number;
+  size: string | null;
+  measurements: TMeasurement[];
+  needPartsForSizeCalc: number[];
+  isSizeStable: boolean;
+  rank: string;
+};
+
+type TSizeMeasurementInputsIndexResponse = {
+  inputs: TMeasurementInput[];
 };
 
 export default function useSizeMeasurementInputsIndex(params: TParams) {
