416d0b97b72fe983fe3545bf54fa870c34d6b736
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-12-03 11:38:31 +0900

    feat: stockingIdInputDialog作成

---
+++ app/_components/item-register/item/stocking-id-input-dialog.tsx
@@ -0,0 +1,73 @@
+import {
+  Box,
+  Button,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { ChangeEvent } from "react";
+import DisableBackDialog from "../../common/dialog/disable-back-dialog";
+
+type TProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  stockingDateCode?: string;
+  stockingSequence?: string;
+  onChangeStockingDateCode: (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  onChangeStockingSequence: (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  onClickAdd: () => void;
+};
+
+export default function StockingIdInputDialog({
+  isOpen,
+  onClose,
+  stockingDateCode,
+  stockingSequence,
+  onChangeStockingDateCode,
+  onChangeStockingSequence,
+  onClickAdd,
+}: TProps) {
+  return (
+    <DisableBackDialog open={isOpen} onClose={onClose}>
+      <DialogTitle>登録コードを入力</DialogTitle>
+      <DialogContent>
+        <Box display="flex" justifyContent="center">
+          <TextField
+            value={stockingDateCode}
+            onChange={onChangeStockingDateCode}
+            variant="standard"
+            type="number"
+            size="small"
+            sx={{ width: "150px" }}
+          />
+          <Typography marginX={1}>-</Typography>
+          <TextField
+            value={stockingSequence}
+            onChange={onChangeStockingSequence}
+            variant="standard"
+            type="number"
+            size="small"
+            sx={{ width: "80px" }}
+          />
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>キャンセル</Button>
+        <Button
+          onClick={onClickAdd}
+          disabled={
+            stockingDateCode !== undefined && stockingSequence !== undefined
+          }
+        >
+          追加
+        </Button>
+      </DialogActions>
+    </DisableBackDialog>
+  );
+}
