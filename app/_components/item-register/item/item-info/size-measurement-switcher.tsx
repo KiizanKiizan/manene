9b9d14fbb18a5eecf3f7c9f9ba1e1b9ef8f534e2
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-12-06 09:09:09 +0900

    fix: switcher層修正

--- app/_components/item-register/item/item-info/size-measurement-switcher.tsx
+++ app/_components/item-register/item/item-info/size-measurement-switcher.tsx
@@ -1,19 +1,89 @@
-import { TSizeMeasurementFetcherProps } from "./size-measurement-fetcher";
+import { TFormOptionIndexResponse } from "@/app/_api/item_register/form/getFormOptionIndex";
+import { TCardsState } from "../item-list/registered-item-card-container";
+import SizeMeasurementFetcher from "./size-measurement-fetcher";
+import SizeMeasurementInputsFetcher from "./size-measurement-inputs-fetcher";
 
-type TProps = TSizeMeasurementFetcherProps & {
-  itemId?: number;
-  adminId?: number;
-  arrivalSize?: string;
+type TProps = {
+  cardState: TCardsState;
+  arrivalSize: string;
+  formOption: TFormOptionIndexResponse;
+  onClose: () => void;
 };
 export default function SizeMeasurementSwitcher({
-  stockingOrderId,
-  itemData,
-  itemImagePath,
-  adminOptions,
-  onCloseItemInfo,
-  itemId,
-  adminId,
+  cardState,
   arrivalSize,
+  formOption,
+  onClose,
 }: TProps) {
-  return <></>;
+  const itemData = [
+    {
+      name: "小カテ",
+      value:
+        formOption.categorySmalls.find(
+          (categorySmall) => categorySmall.value === cardState.cateSmallId
+        )?.name ?? "",
+    },
+    {
+      name: "ブランド",
+      value:
+        formOption.brands.find((brand) => brand.value === cardState.brandId)
+          ?.name ?? "",
+    },
+    {
+      name: "カラー",
+      value:
+        formOption.colors.find((color) => color.value === cardState.colorId)
+          ?.name ?? "",
+    },
+    {
+      name: "サブカラー",
+      value:
+        formOption.colors.find((color) => color.value === cardState.subColorId)
+          ?.name ?? "",
+    },
+    {
+      name: "柄",
+      value:
+        formOption.patterns.find(
+          (pattern) => pattern.value === cardState.patternId
+        )?.name ?? "",
+    },
+    {
+      name: "ロゴ",
+      value:
+        formOption.logos.find((logo) => logo.value === cardState.logoId)
+          ?.name ?? "",
+    },
+    {
+      name: "Dropサイズ",
+      value:
+        formOption.dropSizes.find((drop) => drop.value === cardState.dropSizeId)
+          ?.name ?? "",
+    },
+  ];
+
+  return (
+    <>
+      {cardState.isRegistered ? (
+        <SizeMeasurementFetcher
+          stockingOrderId={cardState.stockingOrderId}
+          itemData={itemData}
+          itemImagePath={cardState.itemImage}
+          adminOptions={formOption.admins}
+          onCloseItemInfo={onClose}
+          itemId={0}
+          adminId={0}
+        />
+      ) : (
+        <SizeMeasurementInputsFetcher
+          stockingOrderId={cardState.stockingOrderId}
+          itemData={itemData}
+          itemImagePath={cardState.itemImage}
+          adminOptions={formOption.admins}
+          onCloseItemInfo={onClose}
+          arrivalSize={arrivalSize}
+        />
+      )}
+    </>
+  );
 }
