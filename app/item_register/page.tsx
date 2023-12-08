d0424fd090ee30e08a459f3e8ea372aa0a4e96bb
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-12-08 20:28:39 +0900

    fix: ダイナミックインポートに変更

--- app/item_register/page.tsx
+++ app/item_register/page.tsx
@@ -1,5 +1,11 @@
+import dynamic from "next/dynamic";
 import getFormOptionIndex from "../_api/item_register/form/getFormOptionIndex";
-import ItemRegisterContainer from "../_components/item-register/item-register-container";
+const ItemRegisterContainer = dynamic(
+  () => import("../_components/item-register/item-register-container"),
+  {
+    ssr: false,
+  }
+);
 
 export default async function ItemRegisterPage() {
   const data = await getFormOptionIndex();
