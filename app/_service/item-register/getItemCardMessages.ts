07a8f62a5457dee3751e5b554590d349dfb5bcac
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-12-10 16:37:41 +0900

    fix: 型修正

--- app/_service/item-register/getItemCardMessages.ts
+++ app/_service/item-register/getItemCardMessages.ts
@@ -43,7 +43,7 @@ export const getRegisteredSizeMessage = ({
   originalSize,
   dropSize,
 }: TMeasurement & {
-  size?: string;
+  size?: string | null;
   originalSize: string;
   dropSize: string;
 }): string => {
